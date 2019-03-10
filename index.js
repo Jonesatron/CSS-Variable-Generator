3355cd64f4e4bec2255e9690b4d7969cd43d9b29
Craig Jones <craigdavidjones@hotmail.co.uk>
2019-03-10 06:13:31 +0000

    Update index.js
    Change functionality to allow multiple instances of the `createVariables` function to be called and not overwrite each other in the root.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const createVariableString = require('./createVariableString');
 function appendVariableStringToHead(baseColor, variableName) {
 	const variableString = createVariableString(baseColor, variableName);
 	const styleTarget = document.getElementById('root-style');
-	styleTarget.innerText = variableString;
+	styleTarget.innerText += variableString;
 }
 
 module.exports = appendVariableStringToHead;
